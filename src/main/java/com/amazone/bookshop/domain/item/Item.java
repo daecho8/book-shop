1fef75bb385163fa537a945a7181eb4683833502
DaeyeonCho <daecho8@gmail.com>
2024-10-26 16:19:08 +0900

    feat: add Item entity

---
+++ src/main/java/com/amazone/bookshop/domain/item/Item.java
@@ -0,0 +1,25 @@
+package com.amazone.bookshop.domain.item;
+
+import com.amazone.bookshop.exception.NotEnoughStockException;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+}
