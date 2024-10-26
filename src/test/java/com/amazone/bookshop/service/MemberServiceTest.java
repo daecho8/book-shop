c9f5c46f8e1c763319a0ae4813b6aaf64175aee1
DaeyeonCho <daecho8@gmail.com>
2024-10-26 16:59:22 +0900

    test: add MemberService test

---
+++ src/test/java/com/amazone/bookshop/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package com.amazone.bookshop.service;
+
+import com.amazone.bookshop.domain.Member;
+import com.amazone.bookshop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("cho");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertThat(member).isEqualTo(memberRepository.findOne(savedId));
+    }
+
+    @Test
+    void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("cho");
+
+        Member member2 = new Member();
+        member2.setName("cho");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+
+}
