5c88633452cba9a3d94977c1aa9e419d5c3f4f3c
DaeyeonCho <daecho8@gmail.com>
2024-10-29 21:00:05 +0900

    feat: add OrderSearch

---
+++ src/main/java/com/amazone/bookshop/repository/OrderSearch.java
@@ -0,0 +1,14 @@
+package com.amazone.bookshop.repository;
+
+
+import com.amazone.bookshop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
